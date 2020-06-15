62169d7def8140f1632d192da9bd55365c71a9ea
JamesTeachingAccount <56122373+JamesTeachingAccount@users.noreply.github.com>
2020-06-15 14:58:24 +0100

    Sequence demo created

---
+++ sequencedemo.py
@@ -0,0 +1,9 @@
+import random
+
+print("I am an example of sequence")
+print("Every time this program is run, the instructions will be processed in the same order")
+print ("though the actual output might not be the same, for example, this number:")
+print (str(random.randint(1,10)))
+print ("will probably be different each time you run the program")
+print ("What's important though is that the ORDER of instructions remains the same")
+        
